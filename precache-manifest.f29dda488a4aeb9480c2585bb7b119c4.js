e41639969d58d237e203c3836f93fab5b9a64c27
Shervan Gheidi <shergheidi@gmail.com>
2021-05-01 14:54:21 -0700

    Updates

---
+++ precache-manifest.f29dda488a4aeb9480c2585bb7b119c4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cedb5e4afb09e88a8840857bdf7919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1216fc5d47b5a3ebcaad",
+    "url": "/static/css/main.e3343b6b.chunk.css"
+  },
+  {
+    "revision": "e670350740b22ec4323b",
+    "url": "/static/js/2.d78a9e47.chunk.js"
+  },
+  {
+    "revision": "26c4ccd93b9d84ea8d038a7dcd8c3ed7",
+    "url": "/static/js/2.d78a9e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1216fc5d47b5a3ebcaad",
+    "url": "/static/js/main.e3e45bf6.chunk.js"
+  },
+  {
+    "revision": "0de845ed36c41eb5f90f",
+    "url": "/static/js/runtime-main.9a5e2046.js"
+  },
+  {
+    "revision": "ea9517107eda990c3babf42beed82757",
+    "url": "/static/media/ChessProgramming.ea951710.jpg"
+  },
+  {
+    "revision": "dcabc8420dcbe5c61345c3e7522a4dec",
+    "url": "/static/media/dfs.dcabc842.png"
+  },
+  {
+    "revision": "727b9a7983f55e41dccb24b8c1262148",
+    "url": "/static/media/game2.727b9a79.gif"
+  },
+  {
+    "revision": "3d559e27bf05896dfc626e91f8f6892f",
+    "url": "/static/media/minimax.3d559e27.png"
+  },
+  {
+    "revision": "b9bfa27dc81d8bdc60af2aafa34fc644",
+    "url": "/static/media/nn.b9bfa27d.png"
+  },
+  {
+    "revision": "a8a4942188ac8adbcf15194f6f8bc656",
+    "url": "/static/media/out.a8a49421.gif"
+  },
+  {
+    "revision": "68f67dd593ec8f6db77fd53b2bbbaaea",
+    "url": "/static/media/pin.68f67dd5.png"
+  },
+  {
+    "revision": "41641ba373ed17a2e4d49ea971d3e4fb",
+    "url": "/static/media/plt.41641ba3.png"
+  },
+  {
+    "revision": "c7e1d0583f00e6c806931b74ba1b744a",
+    "url": "/static/media/resume_web.c7e1d058.pdf"
+  },
+  {
+    "revision": "6420e3488e509dce176a1e957ea07ff5",
+    "url": "/static/media/sieve.6420e348.gif"
+  },
+  {
+    "revision": "1c460e768cb1a57b99fe3262fa4a451c",
+    "url": "/static/media/tetris.1c460e76.gif"
+  }
+]);
